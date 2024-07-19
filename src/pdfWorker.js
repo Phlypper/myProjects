c037754c3a27d74741be519461eef1d04701dd80
Washburn <n0176362@libp45p-lpcvf7ygpq.lan>
2024-07-19 14:32:11 -0400

    Updates

--- src/pdfWorker.js
+++
@@ -1,5 +0,0 @@
-import * as pdfjsLib from 'pdfjs-dist/build/pdf'; 
-console.log('Public URL:', process.env.PUBLIC_URL)
-// Set the workerSrc to the local file path in the public directory 
-
-pdfjsLib.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.mjs`; 
